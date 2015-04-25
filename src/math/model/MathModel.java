2928204e202dcca3588cd58addbc01c9e02b1176
tonynho <tonynho010@gmail.com>
2015-04-25 18:18:48 +0300

    start implementing math model

--- src/math/model/MathModel.java
+++ src/math/model/MathModel.java
@@ -1,5 +1,90 @@
 package math.model;
 
+import statistics.Keys;
+import statistics.Statistics;
+
 public class MathModel {
 
+    private static final char HOME = 'H';
+    private static final char AWAY = 'A';
+    
+    private Statistics stats;
+    private String hostName;
+    private String guestName;
+
+    // iterate through records up to the start of the season
+    private static final String SEASON_START_BOUND = "11/05/14";
+    
+    public MathModel(String hostName, String guestName) {
+        setHostName(hostName);
+        setGuestName(guestName);
+        stats = new Statistics();
+    }
+    
+    static long factorial(int n) {
+        long result = 1;
+        for (int i = n; i > 1; --i) {
+            result *= i;
+        }
+        return result;
+    }
+    
+    static double poisson(int k, double lambda) {
+        return Math.pow(lambda, k) * Math.pow(Math.E, -lambda) / factorial(k);
+    }
+    
+    static double mean(int... numbers) {
+        long sum = 0;
+        for (int i = 0; i < numbers.length; ++i) {
+            sum += numbers[i];
+        }
+        return sum / (double) numbers.length;
+    }
+
+    /**
+     * Calculates the average goals scored in the league at home or as guest.
+     * Records are traversed from the give recordID until the SEASON_START_BOUND,
+     * which marks the end of the last season so we now when this season starts.
+     * 
+     * @param where     : where are the goals scored? At HOME='H' or AWAY='A'.
+     * @param recordID  : recordID in the matches table from which to move back in time
+     * @return  the average goals scored in the league at home or away
+     */
+    double avgGoalsLeague(char where, int recordID) {
+        if (where != HOME && where != AWAY) {
+            throw new IllegalArgumentException("only H or A is accepted as \'where\' argument!");
+        }
+        
+        long sum = 0, count = 0;
+        
+        int recordField = (where == HOME) ? Keys.FTHG.ordinal() : Keys.FTAG.ordinal();
+        
+        for (int i = stats.size() - 1; i > 1 && !(SEASON_START_BOUND.equals( stats.get(i).get(Keys.Date.ordinal())) ); --i) {
+            sum += Integer.valueOf(stats.get(i).get(recordField));
+            ++count;
+        }
+
+        return sum / (double) count;
+    }
+    
+    public String getHostName() {
+        return hostName;
+    }
+
+    public void setHostName(String hostName) {
+        this.hostName = hostName;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+    
+    public Statistics getStats() {
+        return stats;
+    }
+    
 }
---
+++ test/math/model/TestUtilities.java
@@ -0,0 +1,35 @@
+package math.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestUtilities {
+
+    private static final double EPSILON = 1e-14;
+    
+    @Test
+    public void testFactorial() {
+        assertEquals(Long.valueOf(6227020800L), Long.valueOf(MathModel.factorial(13)));
+        assertEquals(Long.valueOf(87178291200L), Long.valueOf(MathModel.factorial(14)));
+    }
+    
+    @Test
+    public void testPoisson() {
+        assertEquals(Double.valueOf(Math.pow(Math.E, -6)), Double.valueOf(MathModel.poisson(0, 6)));
+        assertEquals(Double.valueOf(Math.pow(Math.E, -6) * 6.0), Double.valueOf(MathModel.poisson(1, 6)));
+        assertEquals(Double.valueOf(Math.pow(Math.E, -6) * 6.0 * 6.0 / MathModel.factorial(2)), Double.valueOf(MathModel.poisson(2, 6)));
+        assertEquals(Double.valueOf(Math.pow(Math.E, -6) * Math.pow(6, 3) / MathModel.factorial(3)), Double.valueOf(MathModel.poisson(3, 6)));
+        assertEquals(Double.valueOf(Math.pow(Math.E, -6) * Math.pow(6, 4) / MathModel.factorial(4)), Double.valueOf(MathModel.poisson(4, 6)));   
+
+        assertEquals(Double.valueOf(0.253123435905633), Double.valueOf(MathModel.poisson(1, 2.13)), EPSILON);
+        assertEquals(Double.valueOf(0.238466676192325), Double.valueOf(MathModel.poisson(2, 1.37)), EPSILON);
+    }
+    
+    @Test
+    public void testMean() {
+        assertEquals(Double.valueOf(3.5), Double.valueOf(MathModel.mean(1, 2, 3, 4, 5, 6)));
+        assertEquals(Double.valueOf(42.8), Double.valueOf(MathModel.mean(23, 65, 26, 76, 24)));
+    }
+    
+}
